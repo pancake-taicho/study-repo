6f77504d4b8c0cb1d60bc31c97427d55e72302ed
Michael Metcalf <michaelmetcalf52@gmail.com>
2019-08-10 01:01:50 +0900

    added some solution notes

--- web-developer-bootcamp/049-while.js
+++ web-developer-bootcamp/049-while.js
@@ -1,3 +1,5 @@
+// counter might be a better variable name than num because it's more explicit...
+
 console.log("PRINT ALL NUMBER BETWEEN -10 AND 19");
 var num = -10;
 while(num <= 19) {
@@ -21,6 +23,7 @@ while(num <= 40) {
 //   }
 //   num++;
 // }
+// This solution is actually a little bit better than the above solution since it actually checks if each number is divisible by 2, whereas the first solution just counts up by 2. This works fine ASSUMING the variable num is initialized as an even number. If num had been 11 or any other odd number, though, the code would fail.
 
 console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333");
 var num = 300;
