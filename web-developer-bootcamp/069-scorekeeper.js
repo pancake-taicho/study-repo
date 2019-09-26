8cecf15105b2796b6c1dd14106c415968e7999a8
Michael Metcalf <michaelmetcalf52@gmail.com>
2019-09-26 22:54:29 +0900

    finished the project

--- web-developer-bootcamp/069-scorekeeper.js
+++ web-developer-bootcamp/069-scorekeeper.js
@@ -1,17 +1,56 @@
 var p1Button = document.getElementById("p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input[type='number']");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
-var resetButton = document.querySelector("#reset");
+var gameOver = false;
+var winningScore = numInput.value;
 
 p1Button.addEventListener("click", function(){
-  p1Score++;
+  if(!gameOver){
+    p1Score++;
+    scoreCheck(p1Score, p1Display);
+    }
   p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function(){
-  p2Score++;
+  if(!gameOver){
+    p2Score++;
+    scoreCheck(p2Score, p2Display);
+    }
   p2Display.textContent = p2Score;
-})
+});
+
+resetButton.addEventListener("click", function(){
+  reset();
+});
+
+numInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
+});
+
+function reset(){
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
+
+function scoreCheck(x, y){
+  if(x === winningScore){
+    let winner = y;
+    y.classList.add("winner");
+    gameOver = true;    
+    console.log("GAME OVER!");
+  }
+}
