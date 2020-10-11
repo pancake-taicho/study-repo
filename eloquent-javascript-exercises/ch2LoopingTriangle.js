73ab9d7308274d2d2426254d0cfd34442868df69
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-11 10:43:10 +0900

    added EJS official solution in comments

--- eloquent-javascript-exercises/ch2LoopingTriangle.js
+++ eloquent-javascript-exercises/ch2LoopingTriangle.js
@@ -1,7 +1,19 @@
 var triangleSize = Number(prompt("How tall do you want your triangle to be?"));
+while(isNaN(triangleSize)) {
+  triangleSize = Number(prompt("Please input a number."))
+}
 var triangleBrick = "#";
 
 for(var i = 0; i < triangleSize; i++) {
   console.log(triangleBrick);
   triangleBrick += "#";
 }
+
+/*
+
+Solution in EJS:
+
+for(var line = "#"; line.length < 8; line++)
+  console.log(line);
+
+*/
