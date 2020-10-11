cdf8926fcaeba4e2f5f384f6baec1a06fc0522bf
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-11 10:38:57 +0900

    EJS ch2 exercise: FizzBuzz solution

---
+++ eloquent-javascript-exercises/ch2-exercises/fizzbuzz/ch2FizzBuzz.js
@@ -0,0 +1,20 @@
+var number = Number(prompt("Input your number."));
+
+while(isNaN(number)){
+  number = Number(prompt("Please input a number."));
+}
+
+for(var i = 1; i <= number; i++) {
+  if(i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  else if (i % 3 == 0) {
+    console.log("Fizz");
+  }
+  else if(i % 5 == 0) {
+    console.log("Buzz")
+  }
+  else {
+    console.log(i);
+  }
+}
