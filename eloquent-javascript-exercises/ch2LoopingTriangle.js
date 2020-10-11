a8b8c84f0908ea5a9302013590f7de85d27031fe
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-11 10:00:59 +0900

    solved EJS ch2 Looping a Triangle

---
+++ eloquent-javascript-exercises/ch2LoopingTriangle.js
@@ -0,0 +1,8 @@
+var triangleSize = Number(prompt("How tall do you want your triangle to be?"));
+var triangleBrick = "#";
+
+for(var i = 0; i < triangleSize; i++) {
+  console.log(triangleBrick);
+  triangleBrick += "#";
+}
+
--- eloquent-javascript-exercises/ch2loopingtrianlge.js
+++
@@ -1,9 +0,0 @@
-var triangleSize = Number(prompt("How tall do you want your triangle to be?"));
-
-for(var i = 0; i <= triangleSize; i++) {
-  for (var j = 0; j <= i; j++) {
-    console.log("#");
-    }
-  console.log("\n");
-}
-
