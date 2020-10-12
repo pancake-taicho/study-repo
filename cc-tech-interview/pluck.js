eea60b8b6d5af23b54a58a72f20227b66bc06df0
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-12 18:58:18 +0900

    added the tests to my cc interview problem 'pluck'; the tests output the correct array of answers but then they output 'try again' as if the tests had failed...?

--- cc-tech-interview/pluck.js
+++ cc-tech-interview/pluck.js
@@ -1,10 +1,22 @@
-const dogs = [
+/*
+Declare a function 'pluck'.
+
+'pluck' takes two inputs: an array of objects and a string (the key)
+
+'pluck' returns an array of the corresponding values at that key from each object.
+
+() --> parentheses
+{} --> curly braces/brackets
+[] --> square braces/brackets
+*/
+
+/* const dogs = [
   { name: "moe", age: 4 },
   { name: "larry", age: 5 },
   { name: "curly", age: 6 }
-];
+]; */ 
 
-pluck(dogs, "age");
+// pluck(dogs, "age");
 
 function pluck(arr, key) {
   var keyValues = [];
@@ -14,4 +26,30 @@ function pluck(arr, key) {
   }
   //why does "return keyValues" not work?
   console.log(keyValues);
+}
+
+// Tests
+
+const dogs = [
+  { name: "moe", age: 4 },
+  { name: "larry", age: 5 },
+  { name: "curly", age: 6 }
+];
+
+let actual = pluck(dogs, "name");
+let expected = ["moe", "larry", "curly"];
+
+if (JSON.stringify(actual) === JSON.stringify(expected)) {
+  console.log("Yay!");
+} else {
+  console.log("Try again!");
+}
+
+actual = pluck(dogs, "age");
+expected = [4, 5, 6];
+
+if (JSON.stringify(actual) === JSON.stringify(expected)) {
+  console.log("Yay!");
+} else {
+  console.log("Try again!");
 }
