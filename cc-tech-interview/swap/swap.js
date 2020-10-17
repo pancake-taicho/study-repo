d662ca628c40dec99d120094324c625a7fd001a7
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-17 18:36:47 +0900

    solved cc tech interview problem 'swap'

---
+++ cc-tech-interview/swap/swap.js
@@ -0,0 +1,36 @@
+/*
+Declare a function 'swap'.
+
+'swap' takes one object input.
+
+`swap` returns a new object with each key-value pair swapped.
+
+Keys become values and values become keys.
+
+() --> parentheses
+{} --> curly braces/brackets
+[] --> square braces/brackets
+*/
+
+// Your code here
+
+function swap(Object) {
+  var newObject = {};
+  for (let prop in Object) { // how can you iterate through an object?
+    newObject[Object[prop]] = prop;
+  }
+  return newObject;
+}
+ 
+ // Tests
+ 
+ const testObject = { a: 1, b: 2, c: 3, d: 4 };
+ 
+ const actual = swap(testObject);
+ const expected = { 1: "a", 2: "b", 3: "c", 4: "d" };
+ 
+ if (JSON.stringify(actual) === JSON.stringify(expected)) {
+   console.log("Yay!");
+ } else {
+   console.log("Try again!");
+ }
