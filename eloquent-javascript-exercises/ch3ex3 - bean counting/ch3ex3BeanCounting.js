79607d0b0c9bccaa3db72040b45fcafc92598f23
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-22 20:14:17 +0900

    solved and added solutions to EJS ch3ex3 bean counting

---
+++ eloquent-javascript-exercises/ch3ex3 - bean counting/ch3ex3BeanCounting.js
@@ -0,0 +1,57 @@
+function countChar(string, char) {
+  var charCounter = 0;
+  for(var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == char) {
+      charCounter++;
+    }
+  }
+  return charCounter;
+}
+
+console.log(countChar("kakkerlak", "k"));
+// -> 4
+console.log(countChar("bubblegumballoon", "b"));
+// -> 4
+
+
+/*
+
+First version of "countChar", titled "countBs":
+
+function countBs(string) {
+  var bCount = 0;
+  for(var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "B")
+      bCount++;
+  }
+  return bCount;
+}
+
+console.log(countBs("Babbadook"));
+// -> 1
+console.log(countBs("BaBBadook"));
+// -> 3
+console.log(countBs("BABAYAGA"));
+// -> 2
+
+Solution in EJS:
+
+function countChar(string, ch) {
+  let counted = 0;
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] == ch)
+      counted += 1;
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// -> 2
+console.log(countChar("kakkerlak", "k"));
+// -> 4
+
+*/
