5818f03a4575c5a5c8241fc540faf1bf0a39a0cf
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-23 06:10:56 +0900

    added practice js file for javascriptinfo tutorial

---
+++ experimenting/javscriptInfoTasks.js
@@ -0,0 +1,9 @@
+// alert("I'm JavaScript!");
+// ----
+// let name = "John";
+// let admin = name;
+
+// alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "Mike";
--- experimenting/rowSumOddNumbers.js
+++ experimenting/rowSumOddNumbers.js
@@ -1,10 +1,16 @@
-function rowSumOddNumbers(n) {
-  var value = (n*n) - (n-1);
-  for(var i = 0; i < (value - 1); i++) {
-    value += 2; // <- problem here
-  }
-  return value;
-}
+// function rowSumOddNumbers(n) {
+//   var value = (n*n) - (n-1);
+//   for(var i = 0; i < (value - 1); i++) {
+//     value += 2; // <- problem here
+//   }
+//   return value;
+// }
 
-rowSumOddNumbers(1);
-// rowSumOddNumbers(42);
+// function rowSumOddNumbers(n) {
+//   return Math.pow(n, 3);
+// }
+
+const rowSumOddNumbers = n => Math.pow(n, 3);
+
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(42));
