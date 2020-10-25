c91b4eedd45a434be2bab9dffc29ab0661e9a4f2
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-26 07:15:59 +0900

    did some more scratch pad coding stuff

--- experimenting/javscriptInfoTasks.js
+++ experimenting/javscriptInfoTasks.js
@@ -5,5 +5,14 @@
 
 // alert(admin);
 
-let ourPlanetName = "Earth";
-let currentUserName = "Mike";
+// let ourPlanetName = "Earth";
+// let currentUserName = "Mike";
+// ----
+// let name = "Ilya";
+// alert(`Hello, ${name}!`); 
+// alert(`Hell, ${"name"}!`);
+// alert(`Hell, ${1}!`);
+// ----
+let userName = prompt("What is your name, user?");
+let nameIsCorrect = confirm(`Your name is ${userName}, right?`);
+alert(`Your name is ${userName}!`);
