8b53e7a6657be1d2ecdd0543a96b61add11d5062
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-10-28 07:16:52 +0900

    did more jsinfo tutorial tasks

--- experimenting/javscriptInfoTasks.js
+++ experimenting/javscriptInfoTasks.js
@@ -1,5 +1,7 @@
 // alert("I'm JavaScript!");
+
 // ----
+
 // let name = "John";
 // let admin = name;
 
@@ -7,12 +9,51 @@
 
 // let ourPlanetName = "Earth";
 // let currentUserName = "Mike";
+
 // ----
+
 // let name = "Ilya";
 // alert(`Hello, ${name}!`); 
-// alert(`Hell, ${"name"}!`);
-// alert(`Hell, ${1}!`);
+// alert(`Hello, ${"name"}!`);
+// alert(`Hello, ${1}!`);
+
 // ----
-let userName = prompt("What is your name, user?");
-let nameIsCorrect = confirm(`Your name is ${userName}, right?`);
-alert(`Your name is ${userName}!`);
+
+// let userName = prompt("What is your name, user?");
+// let nameIsCorrect = confirm(`Your name is ${userName}, right?`);
+// alert(`Your name is ${userName}!`);
+
+// ----
+
+// let jsName = prompt("What is the official name of JavaScript?", "");
+// if (jsName == "ECMAScript") {
+//   alert("Right!");
+// } else {
+//   alert("You don't know? It's ECMAScript.");
+// }
+
+// ----
+
+// let num = prompt("Type in a number"); <-- need to reprompt if anything but a number is input
+// if(num > 0) {
+//   alert(1);
+// } else if (num < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+//----
+
+// let result = (a + b < 4) ? 'Below' : 'Over';
+
+// ----
+
+let login = prompt("Enter login");
+
+let message = (login == 'Employee') ? 'Hello':
+  (login == 'Director') ? 'Greetings':
+  (login == '') ? 'No login':
+  '';
+
+alert(message);
