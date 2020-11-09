ff88aa2c935acbb1baae63b0541be1b9c4e4d267
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-11-10 07:05:59 +0900

    did more work on ejs ch4 sum of range exercise

--- eloquent-javascript-exercises/ch4ex1 - sum of a range/ch4SumOfARange.js
+++ eloquent-javascript-exercises/ch4ex1 - sum of a range/ch4SumOfARange.js
@@ -1,9 +1,24 @@
-function range(start, end) {
-  var sum = [];
-  for (var i = 0; i <= (end - start); i++) {
-    sum[i] += i;
+function range(start, end, step) {
+  var range = [];
+  for (var i = 0; i <= Math.abs(end - start); i++) {
+    if (step) {
+      range[i] = start;
+    } else {
+    range[i] = start + i;
+    }
+  }
+  return range;
+}
+
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+
+function sum(arr) {
+  var sum = 0;
+  for(var i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
   return sum;
 }
 
-console.log(range(1, 10));
+console.log(sum(range(1, 10)));
