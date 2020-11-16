eb4d8a6a5bc2bfcb96d6c4357234d56a522f329b
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-11-17 07:03:44 +0900

    made progress on ejs ch4 a list exercise with the help of the hints

--- eloquent-javascript-exercises/ch4ex3 - aList/ch4aList.js
+++ eloquent-javascript-exercises/ch4ex3 - aList/ch4aList.js
@@ -1,15 +1,35 @@
 function arrayToList(array) {
-  let list = {}; // resets 'list' at every iteration, fix this
-  let indexCount = array.length;
-  if (indexCount == 0) {
-    return list;
-  } else {
-    let newObj = {};
-    newObj.value = array[0];
-    array.shift();
-    return arrayToList(array); 
+  let list = {};
+  for(let i = array.length - 1; i >= 0; i--) {
+    if (i == array.length - 1) {
+    list = { value: array[i], rest: null };
+    } else {
+    list = { value: array[i], rest: list };
+    }
   }
+  return list;
 }
 
 let testArray = [1, 2, 3];
 console.log(arrayToList(testArray));
+
+function listToArray(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+// let testList = arrayToList(testArray);
+let testList = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+console.log(listToArray(testList));
