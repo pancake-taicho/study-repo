dfd0b132cc207875d0385a04f6bfa37dee496db9
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-11-18 07:01:25 +0900

    made progress in ejs ch4 list exercise, need to write recursive version of nth

--- eloquent-javascript-exercises/ch4ex3 - aList/ch4aList.js
+++ eloquent-javascript-exercises/ch4ex3 - aList/ch4aList.js
@@ -10,9 +10,6 @@ function arrayToList(array) {
   return list;
 }
 
-let testArray = [1, 2, 3];
-console.log(arrayToList(testArray));
-
 function listToArray(list) {
   let array = [];
   for (let node = list; node; node = node.rest) {
@@ -21,15 +18,30 @@ function listToArray(list) {
   return array;
 }
 
-// let testList = arrayToList(testArray);
-let testList = {
-  value: 1,
-  rest: {
-    value: 2,
-    rest: {
-      value: 3,
-      rest: null
+function prepend(element, list) {
+  let newList = { 'value': element, 'rest': list }
+  return newList;
+}
+
+function nth(list, number) {
+  let node = list;
+  for (let i = 0; i <= number - 1; i++) {
+    if (node) {
+      node = node.rest;
+    } else {
+      return undefined;
     }
   }
-};
+  return node.value;
+}
+
+let testArray = [1, 2, 3];
+console.log(arrayToList(testArray));
+
+let testList = arrayToList(testArray);
 console.log(listToArray(testList));
+
+console.log(prepend(10, arrayToList(testArray)));
+
+console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 6));
