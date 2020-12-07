668dbc69937eb8ec51ff4c7f800f068315271382
Michael Metcalf <michaelmetcalf52@gmail.com>
2020-12-08 06:12:14 +0900

    solved a jsinfo tutorial task, started on shuffle an array

--- experiments-and-practice/javascriptInfo.js
+++ experiments-and-practice/javascriptInfo.js
@@ -2115,3 +2115,37 @@ usersMapped = [
 // alert(arr[0].name); // John
 // alert(arr[1].name); // Mary
 // alert(arr[2].name); // Pete
+
+// TUTORIAL SOLUTION (SAME AS MY SOLUTION):
+
+// function sortByAge(arr) {
+//   arr.sort( (a, b) => a.age - b.age );
+// }
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ john, pete, mary ];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+// ------------------------------------------------------------
+
+// TASK: SHUFFLE AN ARRAY
+
+// write a function 'shuffle(arr)' that shuffles (randomly reorders) elements
+// of the array. multiple runs of 'shuffle' may lead to different orders of elements.
+// all element orders should have an equal probability.
+
+let arr = [1, 2, 3];
+
+function shuffle(arr) {
+  let shuffledArr = [];
+  
+}
