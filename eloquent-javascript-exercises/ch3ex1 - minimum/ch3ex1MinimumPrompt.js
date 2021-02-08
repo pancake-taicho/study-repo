8ae6b8bf637f552b05368d38bb58f9d7b7de48c5
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-02-08 22:20:14 +0900

    added EJS ch3 ex1 and ex2 prompt js files

---
+++ eloquent-javascript-exercises/ch3ex1 - minimum/ch3ex1MinimumPrompt.js
@@ -0,0 +1,7 @@
+/* PROMPT:
+
+The previous chapter introduce the standard function Math.min that returns
+its smallest argument. We can build something like that now. Write a function
+'min' that takes two arguments and returns their minimum.
+
+*/
